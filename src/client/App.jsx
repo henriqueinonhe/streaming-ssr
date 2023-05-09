8167b9f4d664260b61ab386831b6c4644d212456
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-05-09 05:14:41 -0300

    Add global styles to iframe

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -14,6 +14,18 @@ export const App = ({ data }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>SSR React</title>
         <script src="/client/index.js" defer></script>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              * {
+              margin: 0;
+              padding: 0;
+              box-sizing: border-box;
+              position: relative;
+            }
+        `,
+          }}
+        />
       </head>
       <body>
         <script
